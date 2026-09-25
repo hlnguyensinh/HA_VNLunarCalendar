6fc4446edc18f605bb6aec8728f7beb3f11bac30
hlnguyensinh <hl_nguyensinh@yahoo.com>
2026-09-25 08:10:49 +0700

    Commit 2609250810
    Change 15/7 to 15/8

--- dist/vn_lunar_calendar.js
+++ dist/vn_lunar_calendar.js
@@ -1066,7 +1066,6 @@ class VNLunarCalendar extends HTMLElement {
     this._hass = hass;
 
     if (this.getEntity_UseComponent()) {
-      // this.hasService  = hass.config?.components.includes(DOMAIN) || false;
       this.hasService = hass.services?.[DOMAIN]?.[SERVICE_TODAY] || false;
       this.hasService = this.hasService ? true : false;
     } else {
@@ -2008,7 +2007,7 @@ class VNLunarCalendar extends HTMLElement {
         default:
           return this.theme_newyear_day(bg_newyear_day);
       }
-    } else if (dayinfo.lunar.month == 7 && dayinfo.lunar.day == 15) {
+    } else if (dayinfo.lunar.month == 8 && dayinfo.lunar.day == 15) {
       return this.theme_autumn_festival(bg_autumn_night_15);
     } else {
       switch (style) {
